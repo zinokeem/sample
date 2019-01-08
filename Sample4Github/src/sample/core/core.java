095262b9ebb9e22694c48f6a47f4e8420112ac78
znkim <znkim@gaia3d.com>
2019-01-08 17:52:51 +0900

    First Upload

---
+++ Sample4Github/src/sample/core/core.java
@@ -0,0 +1,15 @@
+package sample.core;
+
+public class core {
+
+    public static void main(String[] args) {
+        System.out.println(Const.VERSION);
+    }
+
+}
+
+class Const {
+
+    final static int VERSION = 1;
+
+}
